0bab61312c59164a703a1ea3c0e43bcbd86b0c1c
decanus <7621705+decanus@users.noreply.github.com>
2020-06-29 15:05:35 +0200

    init project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # minima
+
+![Test](https://github.com/decanus/minima/workflows/Test/badge.svg)
+
 MinimaDB: An embeddable database written in Nim.
---
+++ minima.nim
@@ -0,0 +1,5 @@
+# Minima
+#
+# Copyright (c) 2020 Dean Eigenmann
+# Licensed under MIT license ([LICENSE](LICENSE) or http://opensource.org/licenses/MIT)
+
