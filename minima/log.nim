a7f1e6b4a9de28c2b2d3e8b9552454141e2fd58a
decanus <7621705+decanus@users.noreply.github.com>
2020-07-07 17:49:40 +0200

    rename UnencryptedLog -> StandardLog

--- minima/log.nim
+++ minima/log.nim
@@ -9,13 +9,14 @@ type
     Log* = ref object of RootObj
         file: File
 
-    UnencryptedLog* = ref object of Log
-
-method log*(log: Log, key: seq[byte], value: seq[byte]) {.base.} =
-    discard
+    StandardLog* = ref object of Log
+    EncryptedLog* = ref object of Log
 
 method close*(log: Log) {.base.} =
     log.file.close()
+
+method log*(log: Log, key: seq[byte], value: seq[byte]) {.base.} =
+    discard
 
 method next*(log: Log): (seq[byte], seq[byte]) {.base.} =
     discard
@@ -31,22 +32,24 @@ proc readInt(file: File): int =
 
     return int(uint32.fromBytes(arr))
 
-proc init*(T: type UnencryptedLog, file: File): T =
-    result = T(file: file)
-
-method log*(log: UnencryptedLog, key: seq[byte], value: seq[byte]) =
-    let write = concat(
+proc pack(key, value: seq[byte]): seq[byte] =
+    concat(
         @(uint32(len(key)).toBytes),
         @(uint32(len(value)).toBytes),
         key,
         value
     )
 
+proc init*(T: type StandardLog, file: File): T =
+    result = T(file: file)
+
+method log*(log: StandardLog, key: seq[byte], value: seq[byte]) =
+    let write = pack(key, value)
     # @TODO CATCH EXCEPTIONS
     discard log.file.writeBytes(write, 0, len(write))
     log.file.flushFile()
 
-method next*(log: UnencryptedLog): (seq[byte], seq[byte]) =
+method next*(log: StandardLog): (seq[byte], seq[byte]) =
     var keyLen = readInt(log.file)
     var valLen = readInt(log.file)
 
@@ -59,3 +62,9 @@ method next*(log: UnencryptedLog): (seq[byte], seq[byte]) =
     discard log.file.readBytes(val, 0, valLen)
 
     return (key, val)
+
+method log*(log: EncryptedLog, key: seq[byte], value: seq[byte]) =
+    discard
+
+method next*(log: EncryptedLog): (seq[byte], seq[byte]) =
+    discard
