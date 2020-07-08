840ecb63361a109a8ea5ca61cda72a8da74d0af4
decanus <7621705+decanus@users.noreply.github.com>
2020-07-08 14:35:41 +0200

    db tests

--- tests/test_database.nim
+++ tests/test_database.nim
@@ -2,6 +2,9 @@ import unittest
 
 import ../minima/database, stew/[results, byteutils], os, sequtils
 
+type
+    InitProc = proc (): Database
+
 proc checkValues(db: Database, vals: seq[seq[byte]]): bool =
     for val in vals:
         var res = db.get(val)
@@ -10,38 +13,41 @@ proc checkValues(db: Database, vals: seq[seq[byte]]): bool =
 
     return true
 
-suite "Database Test Suite":
-
-    var db: Database
-
+proc createEncryptedDatabase(): Database =
     var key: array[32, byte]
     var pass = "Alice Key"
 
-    setup:
-        copyMem(addr key[0], addr pass[0], len(pass))
-        var result = database.open("/tmp", key)
-        check:
-            result.isOk 
-        db = result.value
+    var res = database.open("/tmp", key)
+    check:
+        res.isOk 
+    
+    return res.value
 
-    teardown:
-        removeFile("/tmp/minima.db")
+proc createDatabase(): Database =
+    var res = database.open("/tmp")
+    check:
+        res.isOk 
+    
+    return res.value
 
-    test "can set and get key":
-        let key = @[byte 1, 2, 3, 4]
-        let value = @[byte 4, 3, 2, 1]
+proc testSetAndGet(fn: InitProc) =
+    let key = @[byte 1, 2, 3, 4]
+    let value = @[byte 4, 3, 2, 1]
         
-        var setResult = db.set(key, value)
-        check:
-            setResult.isOk
+    let db = fn()
+    var setResult = db.set(key, value)
+    check:
+        setResult.isOk
 
-        var getResult = db.get(key)
+    var getResult = db.get(key)
 
-        check:
-            getResult.isOk
-            getResult.value == value
+    check:
+        getResult.isOk
+        getResult.value == value
 
-    test "has returns expected value":
+proc testHasReturnsExpectedValue(fn: InitProc) =
+        let db = fn()
+
         let key = @[byte 1, 2, 3, 4, 5]
         check:
             not db.has(key)
@@ -50,66 +56,96 @@ suite "Database Test Suite":
         check:
             db.has(key)
 
+proc testRecoverWorks(fn: InitProc) =
+    var db = fn()
+    
+    let vals = @[@[byte 1, 2, 3, 4], @[byte 1, 2, 3, 4, 5], @[byte 1, 2, 3, 4, 5, 6]]
+
+    for val in vals:
+        discard db.set(val, val)
+
+    db.close()
+    
+    db = fn()
+
+    check:
+        checkValues(db, vals)
+
+proc testWriteToRecoveredFile(fn: InitProc) =
+    var db = fn()
+    let vals = @[@[byte 1, 2, 3, 4], @[byte 1, 2, 3, 4, 5], @[byte 1, 2, 3, 4, 5, 6]]
+
+    for val in vals:
+        discard db.set(val, val)
+
+    db.close()
+
+    db = fn()
+
+    check:
+        checkValues(db, vals)
+
+    let newVals = @[@[byte 1, 2, 3, 4, 5, 6, 7], @[byte 1, 2, 3, 4, 5, 6, 7, 8]]
+    for val in newVals:
+        discard db.set(val, val)
+        
+    db.close()
+
+    db = fn()
+
+    check:
+        checkValues(db, vals)
+        checkValues(db, newVals)
+
+proc testCanOverWriteValues(fn: InitProc) =
+    let db = fn()
+    let key = @[byte 1, 2, 3, 4]
+
+    discard db.set(key, key)
+
+    check:
+        db.has(key)
+
+    discard db.set(key, @[byte 1, 2])
+
+    var res = db.get(key)
+    check:
+        res.value == @[byte 1, 2]
+
+suite "Encrypted Database Test Suite":
+    teardown:
+        removeFile("/tmp/minima.db")
+
+    test "can set and get key":
+        testSetAndGet(createEncryptedDatabase)
+
+    test "has returns expected value":
+        testHasReturnsExpectedValue(createEncryptedDatabase)
+
     test "recover works":
-        let vals = @[@[byte 1, 2, 3, 4], @[byte 1, 2, 3, 4, 5], @[byte 1, 2, 3, 4, 5, 6]]
-
-        for val in vals:
-            discard db.set(val, val)
-
-        db.close()
-
-        var res = database.open("/tmp", key)
-        check:
-            res.isOk
-
-        db = res.value
-
-        check:
-            checkValues(db, vals)
+        testRecoverWorks(createEncryptedDatabase)
 
     test "writes correctly to recovered file":
-        let vals = @[@[byte 1, 2, 3, 4], @[byte 1, 2, 3, 4, 5], @[byte 1, 2, 3, 4, 5, 6]]
-
-        for val in vals:
-            discard db.set(val, val)
-
-        db.close()
-
-        var res = database.open("/tmp", key)
-        check:
-            res.isOk
-
-        db = res.value
-
-        check:
-            checkValues(db, vals)
-
-        let newVals = @[@[byte 1, 2, 3, 4, 5, 6, 7], @[byte 1, 2, 3, 4, 5, 6, 7, 8]]
-        for val in newVals:
-            discard db.set(val, val)
-        
-        db.close()
-
-        res = database.open("/tmp", key)
-        check:
-             res.isOk
-
-        db = res.value
-
-        check:
-             checkValues(db, vals)
-             checkValues(db, newVals)
+        testWriteToRecoveredFile(createEncryptedDatabase)
 
     test "can overwrite values":
-        let key = @[byte 1, 2, 3, 4]
+        testCanOverWriteValues(createEncryptedDatabase)
 
-        discard db.set(key, key)
+suite "Database Test Suite":
+    teardown:
+        removeFile("/tmp/minima.db")
+        
+    test "can set and get key":
+        testSetAndGet(createDatabase)
 
-        check:
-            db.has(key)
+    test "has returns expected value":
+        testHasReturnsExpectedValue(createDatabase)
 
-        discard db.set(key, @[byte 1, 2])
+    test "recover works":
+        testRecoverWorks(createDatabase)
 
-        var res = db.get(key)
-        check:
-            res.value == @[byte 1, 2]
+    test "writes correctly to recovered file":
+        testWriteToRecoveredFile(createDatabase)
+
+    test "can overwrite values":
+        testCanOverWriteValues(createDatabase)
